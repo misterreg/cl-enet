68e48ca41d5586744f647db6cef49d9159f7eb11
Aaron Clarke <clarkeaa@gmail.com>
2012-08-17 10:41:14 -0700

    started exporting symbols

--- package.lisp
+++ package.lisp
@@ -2,4 +2,20 @@
 
 (defpackage :cl-enet 
   (:use :cl :cffi)
-  (:export :enet-initialize))
+  (:export :enet-initialize
+           :enet-host-create
+           :enet-host-service
+           :run-simple-server
+           ;;
+           :enet-version-major
+           :enet-socket-type-stream
+           :enet-event-type-none
+           :enet-event-type-connect
+           :enet-event-type-disconnect
+           :enet-event-type-receive
+           ;;
+           :enet-address
+           :enet-host
+           :enet-event
+           :enet-packet))
+           
