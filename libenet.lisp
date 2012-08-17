41e239d72cfb5bb743d77438470c0b20e1cbd2eb
Aaron Clarke <clarkeaa@gmail.com>
2012-08-17 14:04:28 -0700

    started sending data

--- libenet.lisp
+++ libenet.lisp
@@ -51,30 +51,7 @@
 ;;;
 ;;;
 
-(defun run-simple-server()
-  (let ((result (enet-initialize)))
-    (format t "initialize:~a~%" result)
-    (with-foreign-object (address 'enet-address)
-      (setf (foreign-slot-value address 'enet-address 'host) 0
-            (foreign-slot-value address 'enet-address 'port) 27275)
-      (let ((host (enet-host-create address 32 4 0 0)))
-        (with-foreign-object (event 'enet-event)
-          (do () (nil)
-            (setf result (enet-host-service host event 1000))
-            (format t "result:~a~%" result)
-            (clear-output)
-            (when (>= result 0)
-              (with-foreign-slots ((event-type) event enet-event)            
-                (cond ((eql event-type 
-                            (foreign-enum-value 'enet-event-type 
-                                                :enet-event-type-connect))
-                       (format t "new connection~%"))
-                      ((eql event-type 
-                            (foreign-enum-value 'enet-event-type 
-                                                :enet-event-type-receive))
-                       (format t "receive data~%"))
-                      ((eql event-type 
-                            (foreign-enum-value 'enet-event-type 
-                                                :enet-event-type-disconnect))
-                       (format t "disconnect~%"))
-                      (t (format t "unknown event!~%")))))))))))
+;; (defmacro with-enet-packet ((name data data-length flags) &rest body)
+;;   `(unwind-protect ,@body
+;;      (format t "hi~%")))
+  
